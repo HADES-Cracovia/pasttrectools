8141b8b2a8e30afbca2b9ccadb46ef41d229e1e7
Rafał Lalik <rafal.lalik@uj.edu.pl>
2018-07-24 09:58:22 +0200

    Make setup() for older setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
 #!/bin/env python3
 
 from setuptools import setup
-setup()
+import re
+
+def vercmp(version1, version2):
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    a = normalize(version1)
+    b = normalize(version2)
+    return ((a>b) - (a<b))
+
+try:
+    import pkg_resources
+except ImportError:
+    print("No setuptools installed for this Python version")
+else:
+    dist = pkg_resources.get_distribution('setuptools')
+
+if vercmp(dist.version, "40.0.0") >= 0:
+    setup()
+else:
+    setup(
+        name='pasttrectools',
+        version='0.1',
+        description='Tools for scanning and configuring pasttrec ASICs',
+        url='https://github.com/HADES-Cracovia/pasttrectools',
+        author='Rafal Lalik',
+        author_email='rafal.lalik@uj.edu.pl',
+        license='MIT',
+        packages=['pasttrec'],
+        install_requires=[
+            'colorama',
+            ],
+        zip_safe=False
+    )
